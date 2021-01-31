6cf52badede159c4c28b20db9438c71ad70dfefe
Max Kliemt <mkrazylp38@gmail.com>
2021-01-31 17:19:50 +0100

    Update GTA_Priv_Lobby.ps1
    Changed Variable Name, because of a set Variable (PID), which cant be overwritten

--- GTA_Priv_Lobby.ps1
+++ GTA_Priv_Lobby.ps1
@@ -5,11 +5,11 @@ $PIDS = get-Process -Name GTA5 | Select-Object Id
 if ($PIDS -ne $empty) {
     Write-Host "This Script will 'empty' your GTA 5 Lobby"
     Write-Host "Pausing process GTA5.exe (PID: $($PIDS.Id))"
-    foreach ($PID in $($PIDS.Id) ) { Pause-Process $PID }
+    foreach ($PIDD in $($PIDS.Id) ) { Pause-Process $PIDD }
     Write-Host "Waiting 10 seconds..."
     Start-Sleep -s 10
     Write-Host "Countinuing GTA5.exe process.."
-    foreach ($PID in $($PIDS.Id) ) { Unpause-Process $PID }
+    foreach ($PIDD in $($PIDS.Id) ) { Unpause-Process $PIDD }
     Write-Host "Your Lobby should now be empty"
 }
 else {
